4281582800eb37c26233c565c05dcfd330ae1e47
PabloE-03 <peibol.max@gmail.com>
2025-02-19 23:13:56 +0100

    Portada para pc

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -15,6 +15,7 @@ import { useRouter } from 'vue-router';
             <div id="c-for"><h3>Formación</h3></div>
             <div id="c-esp"><h3>Especialidades</h3></div>
             <div id="c-pry"><h3>Proyectos</h3></div>
+            <div id="c-red"><h3>Contacto</h3></div>
         </nav>
     </main>
     <footer>
@@ -29,19 +30,61 @@ import { useRouter } from 'vue-router';
 header,main,footer{
     // @include mixings.fondo-portada;
 }
-
+header{
+    width: 100%;
+    height: fit-content;
+    border-color: rgb(56, 52, 56);
+    border-bottom-style: solid;
+    text-align: center;
+    border-width: 5px;
+    .portada{
+            width: 100%;
+            margin: 0% auto;
+            color: rgb(231, 210, 210);
+            h1{
+                font-size: 3vw;
+            }
+            h2{
+                font-size: 2.5vw;
+            }
+            img{
+                width: 20%;
+                border-radius: 50%;
+                border: solid dimgray 4px;
+                margin-top: 2%;
+            }
+        }
+}
+.contenido{
+    width: 100%;
+    height: fit-content;
+    background-color: rgb(44, 42, 44);
+    display: flex;
+    text-align: center;
+    & div{
+        margin: 0% auto;
+        border-left-style: solid;
+        border-right-style: solid;
+        border-color: transparent;
+        transition: 1s;
+        &:hover{
+            padding: 0% 2.1%;
+            border-color: gray;
+            cursor: pointer;
+        }
+    }
+    #c-bio{
+        background-color: gray;
+        color: black;
+        padding: 0% 2.1%;
+    }
+}
 @media screen and (max-width:500px){
     header{
-
         width: 100%;
-        text-align: center;
-        border-bottom-style: solid;
-        border-width: 5px;
-        border-color: rgb(56, 52, 56);
         padding-bottom: 5%;
         .portada{
             width: 100%;
-            margin: 0% auto;
             margin-top: 10%;
             color: rgb(231, 210, 210);
             img{
@@ -50,14 +93,15 @@ header,main,footer{
                 border: solid dimgray 4px;
                 margin-top: 7%;
             }
+            h1{
+                font-size: 6vw;
+            }
+            h2{
+                font-size: 5.5vw;
+            }
         }
     }
     .contenido{
-        width: 100%;
-        height: fit-content;
-        background-color: rgb(44, 42, 44);
-        display: flex;
-        text-align: center;
         & div{
             margin: 0% auto;
             padding: 0% 2.1%;
@@ -69,6 +113,9 @@ header,main,footer{
         #c-bio{
             background-color: gray;
             color: black;
+        }
+        #c-red{
+            display: none;
         }
     }
 }
